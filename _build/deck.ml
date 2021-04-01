63b54159444e6839f7e08b2b752f54a9cae01b11
jn439 <jn439@cornell.edu>
2021-04-01 01:40:46 -0400

    deck.ml,.mli: more specific types, better fxns

--- _build/deck.ml
+++ _build/deck.ml
@@ -1,10 +1,20 @@
 (* Note: You may introduce new code anywhere in this file. *)
 
-type d = None
+type card_id = {
+  name : string;
+  genre : string;
+}
 
-type card_id = string * string * int
+type card_rem = {
+  name : string;
+  copies : int;
+}
 
-type card_rem = string * int
+type d = {
+  cards_used : card_id list;
+  cards_left : card_id list;
+  cards_info : card_rem list;
+}
 
 (* TODO: replace [unit] with a type of your own design. *)
 open Yojson.Basic.Util
@@ -18,3 +28,11 @@ let cards_left d = failwith "unimplemented"
 let cards_used d = failwith "unimplemented"
 
 let cards_info d = failwith "unimplemented"
+
+let draw_card d = failwith "unimplemented"
+
+let peak d = failwith "unimplemented"
+
+let pop d = failwith "unimplemented"
+
+let append d = failwith "unimplemented"
--- _build/deck.mli
+++ _build/deck.mli
@@ -5,19 +5,45 @@
     querying the data. *)
 
 (** The abstract type of values representing the card deck. *)
-type d
+(* type d *)
+
+(* type card_id = string * string * int *)
 
 (** The type of card identifiers represented using three-entry tuples.
     The first entry is the genre of the card, the second entry is the
     kind/name for the card, the third entry is the initial number of
     this kind of card in the deck. *)
-type card_id = string * string * int
+type card_id = {
+  name : string;
+  genre : string;
+}
+
+(* type card_rem = string * int *)
 
 (** The type of card information represented using two-entry tuples. The
     first entry is the kind/name for the card, the second entry is
     remaining number of this kind of card in the deck. *)
-type card_rem = string * int
+type card_rem = {
+  name : string;
+  copies : int;
+}
 
+(** The type representing the card deck. *)
+type d = {
+  cards_used : card_id list;
+  cards_left : card_id list;
+  cards_info : card_rem list;
+}
+
+type player = {
+  hand : card_id list;
+  id : int;
+}
+
+type p = {
+    ai: player list;
+    user: player;
+}
 (** [from_json j] is the card deck that [j] represents. Requires: [j] is
     a valid JSON representation for a deck of cards. *)
 val from_json : Yojson.Basic.t -> d
@@ -40,3 +66,7 @@ val cards_used : d -> card_id list
 (** [cards_info d] is a list of tuples where each tuple pairs a type of
     card with the number of that type of cards left in the deck [d]. *)
 val cards_info : d -> card_rem list
+
+val draw_card : d -> d
+
+val get_hand : p -> card_id list
