70ec9c308881ff0cd701dbbbbd140277cdb8b7b3
jn439 <jn439@cornell.edu>
2021-04-01 23:05:33 -0400

    ms1 install, authors

--- author.ml
+++ author.ml
@@ -1,2 +1,2 @@
 (* TODO: set the value below, then delete this TODO comment. *)
-let hours_worked = 0
+let hours_worked = 15
--- author.mli
+++ author.mli
@@ -1,7 +1,6 @@
 (** CS 3110 Spring 2021 Final Project
 
-    @author [please add your names and netIds here] Chengji Liu (cl2373) *)
-
+    @author Chengji Liu (cl2373), John Ng (jn439), Adrian Chu (ahc249) *)
 
 (** [hours_worked] is the number of hours you worked on this assignment. *)
 val hours_worked : int
